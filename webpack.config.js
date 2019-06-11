473eee10063aa7368f7286a5b39c384ff56fd388
Allan Joshua <calla@amazon.com>
2019-06-11 10:44:19 -0700

    Rename to configurable-layout

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
   output: {
     path: outputDir,
     filename: 'index.js',
-    library: 'layout-manager',
+    library: 'configurable-layout',
     libraryTarget: 'umd'
   },
   module: {
